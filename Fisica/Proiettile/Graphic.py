4e9ae3ada4104240e49a3d15821046ea19702f26
Luca Patruno <failer@LAPTOP-BKI5Q9O3.localdomain>
2021-04-21 11:10:54 +0200

    Risolto problema

--- Fisica/Proiettile/Graphic.py
+++ Fisica/Proiettile/Graphic.py
@@ -1,26 +1,12 @@
 from matplotlib import pyplot as plt
 from pandas import DataFrame, read_csv
 
-def example_plot(ax, fontsize=12, hide_labels=False):
-    ax.plot([1, 2])
-
-    ax.locator_params(nbins=3)
-    if hide_labels:
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-    else:
-        ax.set_xlabel('x-label', fontsize=fontsize)
-        ax.set_ylabel('y-label', fontsize=fontsize)
-        ax.set_title('Title', fontsize=fontsize)
-
 with open("0.csv", "r") as csvfile:
     Angle0 = read_csv(csvfile)
 with open("32.csv", "r") as csvfile:
     Angle32 = read_csv(csvfile)
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
-example_plot(ax1)
-example_plot(ax2)
 
 ax1.plot(Angle0["Time (s)"].tolist(), Angle0["Linear Acceleration x (m/s^2)"].tolist())
 ax1.plot(Angle0["Time (s)"].tolist(), Angle0["Linear Acceleration y (m/s^2)"].tolist())
