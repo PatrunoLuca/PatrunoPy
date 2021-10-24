c749e85b47e9d36ba5c9a46c60149059fb082e13
PatrunoLuca <patruno.luca.s26@liceoviconapoli.it>
2021-10-24 23:53:07 +0200

    Update retta.py

--- coniche/retta.py
+++ coniche/retta.py
@@ -3,23 +3,37 @@ class Retta:
     NumeroRette = 0
     
     def __init__(self, a, b, c):
-        if b == 0:
-            raise ZeroDivisionError
         self._a = float(a)
         self._b = float(b)
         self._c = float(c)
+        if (self._a, self._b) == (0.0, 0.0):
+            raise Exception(f"{self.eq_implicita()} non è una retta")
         self._punti = []
-        self._m = - self._a / self._b
+        if b != 0:
+            self._m = - self._a / self._b
+        else:
+            self._m = None
         Retta.NumeroRette += 1
+
+    def get_a(self):
+        return self._a
+
+    def get_b(self):
+        return self._b
+
+    def get_c(self):
+        return self._c
+
+    def get_m(self):
+        return self._m
+
+    def get_punti(self):
+        return self._punti
 
     def trova_y(self, x=int):
         y = int((self._m * x) + self._c)
         self._punti.append((x, y))
         return y
-
-
-    def lista(self):
-        return self._punti
 
     def coppie(self, start=1, stop=int, step=1):
         return [(x, self.trova_y(x)) for x in range(start, stop, step)]
@@ -27,15 +41,18 @@ class Retta:
     def eq_implicita(self):
         x = f"{self._a}X" if self._a != 1.0 else "X"
         y = f"{self._b}Y" if self._b != 1.0 else "Y"
-        segno = "+" if self._c > 0 else "-"
-        c = f"{segno} {abs(self._c)}" if self._c != 0.0 else "" 
-        return f"{x} + {y}{c} = 0"
+        segno = "+" if self._c >= 0 else "-"
+        c = f"{segno} {abs(self._c)}"
+        return f"{x} + {y} {c} = 0"
 
     def eq_esplicita(self):
         segno = "+" if self._c > 0 else "-"
-        c = f"{segno} {abs(self._c)}" if self._c != 0.0 else "" 
-        x = f" {self._m}X " if self._m != 0.0 else ""
-        return f"Y ={self._m} {c}"
+        c = f"{segno} {abs(self._c)}"
+        x = f"{self._m}X "
+        if (c, x) != ("+ 0.0","0.0X"):
+            return f"Y = {self._m} {c}" 
+        else:
+            return "Y = 0"
     
     @classmethod
     def intersezione(cls, r1, r2):
@@ -49,6 +66,22 @@ class Retta:
             return "Perpendicolari"
         else:
             return "Incidenti"
+
+    #WIP funzione che crea un oggetto retta a partire da una lista di punti
+    @classmethod
+    def from_punti(cls, punti):
+        assert len(punti) == 2
+        assert type(punti) == list
+        a,b,c= [1, 2, 3]
+        return cls(a, b, c)
+    
+    #WIP funzione che crea un oggetto retta a partire dal coefficiente angolare ed un punto
+    @classmethod
+    def from_coeff(cls, m, punto):
+        assert len(punto) == 2
+        assert type(punto) == tuple
+        a,b,c= [1, 2, 3]
+        return cls(a, b, c)
  
 
 if __name__ == "__main__":
